53b7432a7ff086b3f774fe78d867a24a78a92d7c
Martin <mpatino117@gmail.com>
2025-01-07 17:20:16 -0700

    Add multilingual greeting functionality with a phrasebook

--- chapter_2/2.4/main.go
+++ chapter_2/2.4/main.go
@@ -10,5 +10,28 @@ func main() {
 	flag.StringVar(&lang, "lang", "en", "The langue is required")
 	flag.Parse()
 
-	fmt.Printf("Key %q", lang)
+	greeting := greet(language(lang))
+
+	fmt.Printf("greeting %q", greeting)
+}
+
+type language string
+
+var phrasebook = map[language]string{
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello world",       // English
+	"fr": "Bonjour le monde",  // French
+	"he": "שלום עולם",         // Hebrew
+	"ur": "ہیلو دنیا",         // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
+}
+
+// greet says hello to the world
+func greet(l language) string {
+	greeting, ok := phrasebook[l]
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
+	}
+
+	return greeting
 }
